aa933c10791ab29b53b0285f1f8938f8843f2254
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-05-17 14:15:44 -0300

    adicionar mensagem de erro

--- Js/index.js
+++ Js/index.js
@@ -13,7 +13,31 @@ class ValidaForm {
 
     handleSubmit(event) {
         event.preventDefault();
+        const campoValido = this.isValid();
+    }
+
+    isValid() {
+        let valid = true;
+        const campos = document.querySelectorAll('.valid');
+        const erro = document.querySelectorAll('.erro-text');
+        erro.forEach(e => {
+            e.remove();
+        });
         
+        campos.forEach(campo => {
+            let label = campo.previousElementSibling.innerText;
+            label = label.replace(":", '');
+            if(!campo.value) {
+                this.criaErro(campo, `Campo ${label} não pode estar em branco.`);
+            }
+        });
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('erro-text');
+        campo.insertAdjacentElement('afterend', div);
     }
 }
 
